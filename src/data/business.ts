b4c143399a6ff2a8ecacf6ff69b261dc9cd232d3
cresyno <taiwoabraham640@gmail.com>
2026-08-14 06:52:16 +0100

    Add business information to business.ts

---
+++ src/data/business.ts
@@ -0,0 +1,8 @@
+export const business = {
+  legalName: "OB Designs & Interiors LTD",
+  address: "67 Adesan Rd, Mowe, Obafemi Owode 110113, Ogun State",
+  phoneDisplay: "0813 192 9791",
+  phoneWhatsApp: "2348131929791",
+  email: "Obinteriors555@gmail.com",
+  yearsActive: 6,
+};
