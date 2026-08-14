9788c2c53ba14eddb7af365722a9d30d7b0cfd9c
cresyno <taiwoabraham640@gmail.com>
2026-08-14 07:47:03 +0100

    Refactor navigation to use Nav component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import Contact from "./pages/Contact";
@@ -6,11 +7,7 @@ import Contact from "./pages/Contact";
 export default function App() {
   return (
     <BrowserRouter>
-      <nav style={{ padding: "1rem", display: "flex", gap: "1rem", fontFamily: "sans-serif" }}>
-        <Link to="/">Home</Link>
-        <Link to="/shop">Shop</Link>
-        <Link to="/contact">Contact</Link>
-      </nav>
+      <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
