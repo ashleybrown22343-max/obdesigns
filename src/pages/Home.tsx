c8738aedf801e292afb836a943377b5fb8867ff1
cresyno <taiwoabraham640@gmail.com>
2026-08-14 13:01:59 +0100

    Add custom document title to Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,8 +7,11 @@ import PaintStory from "../components/PaintStory";
 import Process from "../components/Process";
 import Footer from "../components/Footer";
 import StickyCTA from "../components/StickyCTA";
+import { useDocumentTitle } from "../lib/useDocumentTitle";
 
 export default function Home() {
+  useDocumentTitle("OB Designs & Interiors | Painting, POP, Interiors & Paint");
+
   return (
     <>
       <Hero />
