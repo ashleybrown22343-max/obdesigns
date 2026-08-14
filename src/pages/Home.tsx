2b0deaf7b84daa6e9c83e84390ff56125a486748
cresyno <taiwoabraham640@gmail.com>
2026-08-14 07:36:17 +0100

    Add new components to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,10 @@ import FeaturedWork from "../components/FeaturedWork";
 import Services from "../components/Services";
 import WhyOB from "../components/WhyOB";
 import AboutOB from "../components/AboutOB";
+import PaintStory from "../components/PaintStory";
+import Process from "../components/Process";
+import ProjectConsultation from "../components/ProjectConsultation";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
@@ -12,6 +16,10 @@ export default function Home() {
       <Services />
       <WhyOB />
       <AboutOB />
+      <PaintStory />
+      <Process />
+      <ProjectConsultation />
+      <Footer />
     </>
   );
 }
