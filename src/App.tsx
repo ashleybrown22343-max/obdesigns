3e2e336e175ea2808d7ab9aa9c0b19e8c55f587a
ashleybrown22343-max <ashleybrown22343@gmail.com>
2026-08-23 22:00:20 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
-import Shop from "./pages/Shop";
 import Projects from "./pages/Projects";
 import Consultation from "./pages/Consultation";
 import Contact from "./pages/Contact";
+import ServicesHub from "./pages/ServicesHub";
+import ServiceDetail from "./pages/ServiceDetail";
 import NotFound from "./pages/NotFound";
 
 export default function App() {
@@ -13,8 +14,15 @@ export default function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/services" element={<ServicesHub />} />
+        <Route path="/painters-in-nigeria" element={<ServiceDetail slug="painters-in-nigeria" />} />
+        <Route path="/house-painting-services-nigeria" element={<ServiceDetail slug="house-painting-services-nigeria" />} />
+        <Route path="/exterior-painting-nigeria" element={<ServiceDetail slug="exterior-painting-nigeria" />} />
+        <Route path="/interior-designers-in-nigeria" element={<ServiceDetail slug="interior-designers-in-nigeria" />} />
+        <Route path="/pop-ceiling-installation-nigeria" element={<ServiceDetail slug="pop-ceiling-installation-nigeria" />} />
+        <Route path="/floor-screeding-nigeria" element={<ServiceDetail slug="floor-screeding-nigeria" />} />
+        <Route path="/new-wave-emulsion-paint-nigeria" element={<ServiceDetail slug="new-wave-emulsion-paint-nigeria" />} />
         <Route path="/projects" element={<Projects />} />
-        <Route path="/shop" element={<Shop />} />
         <Route path="/consultation" element={<Consultation />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<NotFound />} />
